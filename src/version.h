4de14ad417f9fa0583fdafc9cf30f218461e3cfd
Joachim Wiedorn <ad_debian@joonet.de>
2010-06-06 21:35:56 +0200

    Update for file CHANGELOG

--- src/version.h
+++ src/version.h
@@ -4,9 +4,9 @@
 #define VERSION_H
 			/* retail */
 /*#define DEBUG_PARTITIONS 1 */
-#define VERSION_MAJOR 22
-#define VERSION_MINOR 8
+#define VERSION_MAJOR 23
+#define VERSION_MINOR 0
 #define VERSION_EDIT  ""
-#define VERSION_DATE "19-Feb-2007"
+#define VERSION_DATE "unreleased"
 
 #endif
