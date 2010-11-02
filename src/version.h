282d0b8642ae127585021812fcde8cde2a05d99c
Joachim Wiedorn <ad_debian@joonet.de>
2010-11-02 09:31:43 +0100

    Update changelog for next version

--- src/version.h
+++ src/version.h
@@ -2,11 +2,10 @@
 
 #ifndef VERSION_H
 #define VERSION_H
-			/* retail */
-/*#define DEBUG_PARTITIONS 1 */
+
 #define VERSION_MAJOR 23
-#define VERSION_MINOR 0
+#define VERSION_MINOR 1
 #define VERSION_EDIT  ""
-#define VERSION_DATE "28-June-2010"
+#define VERSION_DATE "unreleased"
 
 #endif
